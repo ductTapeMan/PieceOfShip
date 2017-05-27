a43d189847057782e5eb9bd5926dfa207c85e06b
Breanna Wilson <wilson4653@saskpolytech.ca>
2017-05-26 23:18:14 -0600

    Initial Push

---
+++ CrewClasses/CrewClasses/Crew.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewClasses
+{
+    public abstract class Crew
+    {
+        protected int level;
+        protected int firingSpeed;
+        protected int range;
+        protected string shipLocation;
+        protected string nationality;
+        protected int experience;
+        protected string querk;
+        protected string skill;
+
+        public abstract int levelUp();
+        public abstract Crew levelToClass();
+    }
+}
